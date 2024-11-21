951baf29db0c286a9e91f22b5f6347d78c2e2525
Martyn Janes <martyn@obany.com>
2024-11-21 12:25:16 +0200

    Improve vitest config with nameof plugin

--- packages/ui-tailwind/vitest.config.ts
+++ packages/ui-tailwind/vitest.config.ts
@@ -1,26 +1,20 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
-import typescript from "rollup-plugin-typescript2";
+import { NameOfPlugin } from "@twin.org/nameof-vitest-plugin";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	esbuild: false,
-	plugins: [
-		typescript({
-			tsconfig: "./tests/tsconfig.json",
-			clean: true
-		})
-	],
+	plugins: [NameOfPlugin],
 	test: {
 		include: ["./tests/**/*.spec.ts"],
 		globals: true,
-		bail: 1,
 		testTimeout: 300000,
 		hookTimeout: 300000,
+		bail: 1,
 		coverage: {
-			provider: "v8",
 			reporter: ["text", "lcov"],
-			exclude: ["**/index.ts", "**/models/**/*.ts", "**/tests/**/*.ts"]
+			include: ["src/**/*.ts"],
+			exclude: ["**/index.ts", "**/models/**/*.ts"]
 		},
 		fileParallelism: false
 	}
