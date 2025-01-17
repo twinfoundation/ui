5e7c4c0c86ce6e316c4fd27d8327010eba955f9a
Adrian Sanchez Sequeira <adrian.sanchez.sequeira@iota.org>
2025-01-17 08:51:01 +0100

    fix: Format and lint

--- packages/ui-components-react/src/sidebar/sidebar.tsx
+++ packages/ui-components-react/src/sidebar/sidebar.tsx
@@ -1,10 +1,16 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
+import { Sidebar as FlowbiteSidebar, Badge as FlowbiteBadge } from "flowbite-react";
 import {
-	Sidebar as FlowbiteSidebar,
-	Badge as FlowbiteBadge
-} from "flowbite-react";
-import { ArrowRight, ChartPie, CircleMinus, CirclePlus, DotsVertical, QuestionCircle, ShoppingBag, TableColumn } from "flowbite-react-icons/outline";
+	ArrowRight,
+	ChartPie,
+	CircleMinus,
+	CirclePlus,
+	DotsVertical,
+	QuestionCircle,
+	ShoppingBag,
+	TableColumn
+} from "flowbite-react-icons/outline";
 import React, { type ReactNode } from "react";
 import { SidebarPropTypes, type SidebarProps } from "./sidebarProps";
 
@@ -38,7 +44,11 @@ export class Sidebar extends React.Component<SidebarProps> {
 	public render(): ReactNode {
 		return (
 			<FlowbiteSidebar aria-label="Sidebar with logo branding example">
-				<FlowbiteSidebar.Logo href="#" img="https://flowbite-react.com/favicon.svg" imgAlt="Flowbite logo">
+				<FlowbiteSidebar.Logo
+					href="#"
+					img="https://flowbite-react.com/favicon.svg"
+					imgAlt="Flowbite logo"
+				>
 					Flowbite
 				</FlowbiteSidebar.Logo>
 				<FlowbiteSidebar.Items>
@@ -52,7 +62,7 @@ export class Sidebar extends React.Component<SidebarProps> {
 							renderChevronIcon={(theme, open) => {
 								const IconComponent = open ? CircleMinus : CirclePlus;
 
-								return <IconComponent/>;
+								return <IconComponent />;
 							}}
 						>
 							<FlowbiteSidebar.Item href="#">Products</FlowbiteSidebar.Item>
@@ -78,37 +88,36 @@ export class Sidebar extends React.Component<SidebarProps> {
 				</FlowbiteSidebar.Items>
 				<FlowbiteSidebar.CTA>
 					<div className="mb-3 flex items-center">
-					<FlowbiteBadge color="warning">Beta</FlowbiteBadge>
-					<button
-						aria-label="Close"
-						className="-m-1.5 ml-auto inline-flex h-6 w-6 rounded-lg bg-gray-100 p-1 text-cyan-900 hover:bg-gray-200 focus:ring-2 focus:ring-gray-400 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
-						type="button"
-					>
-						<svg
-						aria-hidden
-						className="h-4 w-4"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
+						<FlowbiteBadge color="warning">Beta</FlowbiteBadge>
+						<button
+							aria-label="Close"
+							className="-m-1.5 ml-auto inline-flex h-6 w-6 rounded-lg bg-gray-100 p-1 text-cyan-900 hover:bg-gray-200 focus:ring-2 focus:ring-gray-400 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
+							type="button"
 						>
-						<path
-							fillRule="evenodd"
-							d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-							clipRule="evenodd"
-						/>
-						</svg>
-					</button>
+							<svg
+								aria-hidden
+								className="h-4 w-4"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fillRule="evenodd"
+									d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+									clipRule="evenodd"
+								/>
+							</svg>
+						</button>
 					</div>
 					<div className="mb-3 text-sm text-cyan-900 dark:text-gray-400">
-					Preview the new Flowbite dashboard navigation! You can turn the new
-					navigation off for a limited time in your
-					profile.
+						Preview the new Flowbite dashboard navigation! You can turn the new navigation off for a
+						limited time in your profile.
 					</div>
 					<a
-					className="text-sm text-cyan-900 underline hover:text-cyan-800 dark:text-gray-400 dark:hover:text-gray-300"
-					href="#"
+						className="text-sm text-cyan-900 underline hover:text-cyan-800 dark:text-gray-400 dark:hover:text-gray-300"
+						href="#"
 					>
-					Turn new navigation off
+						Turn new navigation off
 					</a>
 				</FlowbiteSidebar.CTA>
 			</FlowbiteSidebar>
