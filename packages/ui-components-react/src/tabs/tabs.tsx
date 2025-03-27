b067c48c394d0d6c174f8bffaa2b3779d0df3694
Albert Olivé <albertolivecorbella@gmail.com>
2025-03-27 15:31:34 +0100

    fix: small issue

--- packages/ui-components-react/src/tabs/tabs.tsx
+++ packages/ui-components-react/src/tabs/tabs.tsx
@@ -5,29 +5,6 @@ import { memo, useRef, useCallback, type JSX } from "react";
 import type { TabsProps } from "./tabsProps";
 import { TABS_THEME_CONFIG } from "./tabsTheme";
 
-const TABS_THEME_CONFIG = {
-	base: "flex flex-col gap-2",
-	tablist: {
-		base: "flex w-full text-center",
-		variant: {
-			underline: "-mb-px flex-wrap border-b border-surface-primary dark:surface-primary-dark"
-		},
-		tabitem: {
-			base: "flex flex-1 items-center justify-center text-sm p-4 font-medium first:ml-0 focus:outline-none disabled:cursor-not-allowed disabled:text-tertiary disabled:dark:text-tertiary-dark",
-			variant: {
-				underline: {
-					base: "",
-					active: {
-						on: "font-bold relative after:absolute after:bottom-[-2px] after:left-1/4 after:w-1/2 after:border-b-2 after:border-brand-primary text-brand-primary",
-						off: "border-b-2 border-transparent text-tertiary hover:border-gray-300 hover:text-gray-600"
-					}
-				}
-			},
-			icon: "mr-2 h-5 w-5"
-		}
-	}
-} as const;
-
 /**
  * Tabs component.
  * Renders a tabbed interface that supports icons, disabled states, and controlled/uncontrolled modes.
