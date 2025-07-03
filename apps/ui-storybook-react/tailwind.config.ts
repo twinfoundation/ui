551f2fbe03af77e26fd2218415e6498b8ebec3fa
gianpaj <gianpa@gmail.com>
2025-07-03 12:47:48 +0200

    wip remove '@twin.org/ui-components-react' imports

--- apps/ui-storybook-react/tailwind.config.ts
+++ apps/ui-storybook-react/tailwind.config.ts
@@ -1,19 +1,19 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
-import { execSync } from "node:child_process";
-import { TailwindConfig } from "@twin.org/ui-components-react";
+// import { execSync } from "node:child_process";
+// import { TailwindConfig } from "@twin.org/ui-components-react";
 
-const npmRoot = execSync("npm root").toString().trim().replace(/\\/g, "/");
+// const npmRoot = execSync("npm root").toString().trim().replace(/\\/g, "/");
 
 export default {
 	content: [
 		"./src/**/*.{js,ts,jsx,tsx}",
-		"./.storybook/**/*.html",
-		...TailwindConfig.getContentPaths(npmRoot)
+		"./.storybook/**/*.html"
+		// ...TailwindConfig.getContentPaths(npmRoot)
 	],
-	plugins: TailwindConfig.getPlugins(),
+	// plugins: TailwindConfig.getPlugins(),
 	darkMode: "class",
 	theme: {
-		extend: TailwindConfig.getTheme()
+		// extend: TailwindConfig.getTheme()
 	}
 };
--- packages/ui-components-react/tailwind.config.ts
+++ packages/ui-components-react/tailwind.config.ts
@@ -1,15 +1,18 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
-import { execSync } from "node:child_process";
-import { TailwindConfig } from "./src/config/tailwindConfig";
+// import { execSync } from "node:child_process";
+// import { TailwindConfig } from "./src/config/tailwindConfig";
 
-const npmRoot = execSync("npm root").toString().trim().replace(/\\/g, "/");
+// const npmRoot = execSync("npm root").toString().trim().replace(/\\/g, "/");
 
 export default {
-	content: ["./src/**/*.{js,ts,jsx,tsx}", ...TailwindConfig.getContentPaths(npmRoot)],
-	plugins: TailwindConfig.getPlugins(),
+	content: [
+		"./src/**/*.{js,ts,jsx,tsx}"
+		// ...TailwindConfig.getContentPaths(npmRoot)
+	],
+	// plugins: TailwindConfig.getPlugins(),
 	darkMode: "class",
 	theme: {
-		extend: TailwindConfig.getTheme()
+		// extend: TailwindConfig.getTheme()
 	}
 };
